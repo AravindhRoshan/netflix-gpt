b88fd22aa5d78d2518d3e2933e99b4adc1728bc5
Roshan <aravindhroshan2k1@gmail.com>
2024-07-08 21:00:34 +0530

    Icons installation

--- src/screens/LandingPage/components/MovieListContainer/components/MovieListCategories.js
+++ src/screens/LandingPage/components/MovieListContainer/components/MovieListCategories.js
@@ -4,13 +4,7 @@ import MovieCards from "./MovieCards";
 const MovieListCategories = (props) => {
   const { title, movieData } = props;
   console.log("movieData", movieData);
-  const scrollRef = useRef(null);
-
-  const scrollRight = () => {
-    if (scrollRef.current) {
-      scrollRef.current.scrollBy({ left: 300, behavior: "smooth" });
-    }
-  };
+ 
   return (
     <div className="px-3">
       <h1 className="text-white font-bold text-4xl p-4 ">
@@ -22,14 +16,6 @@ const MovieListCategories = (props) => {
             movieData.map((movie, index) => (
               <MovieCards key={movie.id} posterPath={movie.poster_path} />
             ))}
-          <div className="flex items-center justify-center min-w-[150px] bg-gray-800">
-            <button
-              onClick={scrollRight}
-              className="text-white p-2 rounded-full"
-            >
-              Arrow
-            </button>
-          </div>
         </div>
       </div>
     </div>
